847f2aea739488f5686e33394e649c6e44e44d8b
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-09-14 14:06:33 -0600

    Director.prepareForRecording

--- src/hollywoo/Director.hx
+++ src/hollywoo/Director.hx
@@ -102,6 +102,8 @@ interface Director<Set, Actor, Sound, Song, Prop, VoiceTrack, Camera, LightSourc
 
     function rollCredits(credits:Array<CreditsLine>, cc:Continuation, ?timeLimit:Float):Void;
 
+    function prepareForRecording():Void;
+
     function doLoading(_load:Array<Void->Void>, scavenged:Bool, cc:Continuation):Void;
     function cleanup():Void;
 }
